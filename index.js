5cb19859d24367038e1e0cfb1bc72c7b5b806062
Justin <justinvosnz@gmail.com>
2020-07-06 21:33:16 +1200

    Fix implementation

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
-const moment = require('moment')
+const NUM_SECONDS_IN_DAY = 24 * 60 * 60;
+const SWEDISH_OFFSET = (60 * 60) / NUM_SECONDS_IN_DAY * 1000;
 
-function getMomentNow() {
-	return moment(Date.now()).utcOffset(1)
+function dotbeat() {
+  const now = Date.now();
+  const nowDate = new Date(now);
+  const startOfDay = Date.UTC(nowDate.getUTCFullYear(), nowDate.getUTCMonth(), nowDate.getUTCDate());
+
+  const elapsedMilliseconds = now - startOfDay;
+  const beats = elapsedMilliseconds / NUM_SECONDS_IN_DAY + SWEDISH_OFFSET;
+
+  return beats;
 }
-
-function formatTwoDecimalPlace(value) {
-	let beats = Math.floor(value)
-	let remainder = Math.round(value % 1 * 100)
-	let strRemainder = String(remainder)
-	if(remainder == 0) {
-		strRemainder = "00"
-	} else if(remainder < 10) {
-		strRemainder = "0" + remainder
-	}
-	return String(beats) + "." + strRemainder
-}
-
-function get(format="number", subbeats=false) {
-	let momentNow = getMomentNow()
-	let momentDayStart = getMomentNow()
-	momentDayStart.hour(0)
-	momentDayStart.minute(0)
-	momentDayStart.second(0)
-	momentDayStart.milliseconds(0)
-	let millisecondsToday = momentNow.diff(momentDayStart)
-	let beatTime = Math.floor(millisecondsToday / 86400)
-	if(subbeats) {
-		beatTime = Math.floor(millisecondsToday / 864) / 100
-	}
-
-	if(format === "number") {
-		return beatTime
-	} else if(format === "string") {
-		if(subbeats) {
-			return "@" + formatTwoDecimalPlace(beatTime)
-		}
-		return "@" + beatTime.toString()
-	} else {
-		return beatTime
-	}
-}
-
-exports.get = get
